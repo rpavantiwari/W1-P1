c4a60cae12dfe7f730c046c5170698fef48ced3e
rpavantiwari <golutiwarijamuni@gmail.com>
2024-09-25 13:06:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// setTimeout(function(){
+//     console.log("Pavan Tiwari");
+// },2000);
+
+// const sayPavan = function() {
+//     console.log("Pavan Tiwari");
+// }
+// const changeText = function() {
+//     document.querySelector('#js').
+//     innerText = "Pavan Tiwari*x";
+// }
+// setTimeout(changeText,2000);
+// setTimeout(prompt(sayPavan), 2000);
+// const sayPavan = function() {
+//     console.log("Pavan Tiwari");
+// }
+// const changeText = function() {
+//     document.querySelector('#js').innerHTML = "Pavan Tiwari"
+// }
+// const changeMe = setTimeout(changeText, 2000)
+// document.querySelector('#btn2').addEventListener('click', function(){
+// clearTimeout(changeMe)
+// console.log("Stopped")
+// })
